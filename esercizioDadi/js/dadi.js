efaaf606c5d13a6eb96d0707a0c7922cb4ea0fe8
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-03-22 17:33:43 +0100

    Dadi finito

---
+++ esercizioDadi/js/dadi.js
@@ -0,0 +1,28 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+// Math.random();
+
+// Math.nomeVariabile(Math.random() * 6) + 1;
+
+
+//generare 2 numeri in 2 variabili
+var generatoUtente;
+var generatoComputer;
+
+
+generatoUtente = Math.floor(Math.random() * 6) + 1;
+console.log("numero utente", generatoUtente);
+
+generatoComputer = Math.floor(Math.random() * 6) + 1;
+console.log("numero computer", generatoComputer);
+
+//confrontare quale tra i due è il maggiore
+
+if (generatoUtente > generatoComputer) {
+    alert("hai vinto!");
+} else {
+    alert("ha vinto il computer :(");
+}
+
